f93e29f22f0ccd4410d37ed88345644a546d2809
Nikita G <jobnikitka152rus@gmail.com>
2023-05-01 10:08:10 +0300

    add bonus programm

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15000;
+        int bonus = 20;
+        int bonusMiles = ticketPrice / bonus;
+        System.out.println("Баланс бонусных миль: " + bonusMiles);
+    }
+}
